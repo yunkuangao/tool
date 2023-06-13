2a5b6e278fd9ead7cedcd451101b312a9f49dd8a
yun <yun@yka.moe>
2023-06-13 17:14:25 +0800

    这个本地能跑通了,后面在测试github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
+
 plugins {
     alias(libs.plugins.jvm)
-//    `maven-publish`
-    `java-library`
-//    alias(libs.plugins.dokka)
     signing
-    alias(libs.plugins.publish)
+    `maven-publish`
 }
 
 group = "moe.yka"
@@ -12,12 +10,6 @@ version = "0.1.0"
 
 dependencies {
     implementation(libs.kotlin.stdlib)
-
-    testImplementation(testLibs.kotlin.test)
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 kotlin {
@@ -28,36 +20,70 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
+
+// publish maven repo
 publishing {
-    repositories {
-        maven {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "tool"
             version = System.getenv("VERSION")
-            name = "githubPackages"
+
+            from(components["java"])
+
+            pom {
+                name.set("tool")
+                description.set("tool")
+                inceptionYear.set("2023")
+                url.set("https://github.com/yunkuangao/tool")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yunkuangao")
+                        name.set("yun")
+                        url.set("https://github.com/yunkuangao/")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/yunkuangao/tool/")
+                    connection.set("scm:git:git://github.com/yunkuangao/tool.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/yunkuangao/tool.git")
+                }
+            }
+        }
+    }
+    repositories {
+        // Maven Central
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername")
+                password = System.getenv("ORG_GRADLE_PROJECT_mavenCentralPassword")
+            }
+        }
+        // GitHub Release
+        maven {
+            name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/yunkuangao/tool")
-            credentials(PasswordCredentials::class)
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
 
-// publish maven repo
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            groupId = project.group.toString()
-//            artifactId = "tool"
-//            version = System.getenv("VERSION")
-//            from(components["java"])
-//        }
-//    }
-//    repositories {
-//        // GitHub Release
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/yunkuangao/tool")
-//            credentials {
-//                username = System.getenv("GITHUB_ACTOR")
-//                password = System.getenv("GITHUB_TOKEN")
-//            }
-//        }
-//    }
-//}
+signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["maven"])
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,22 +24,10 @@ dependencyResolutionManagement {
 
             // plugin
             plugin("jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
-            plugin("dokka", "org.jetbrains.dokka").versionRef("kotlin")
             plugin("publish", "com.vanniktech.maven.publish").versionRef("publish")
 
             // dependency
             library("kotlin-stdlib", "org.jetbrains.kotlin", "kotlin-stdlib").versionRef("kotlin")
-        }
-
-        create("testLibs") {
-
-            val kotlinVersion = extra["kotlinVersion"] as String
-
-            // version
-            version("kotlin", kotlinVersion)
-
-            // testImplementation
-            library("kotlin-test", "org.jetbrains.kotlin", "kotlin-test").versionRef("kotlin")
         }
     }
 }
